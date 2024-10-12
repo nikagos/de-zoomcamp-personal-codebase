092b04f008be855556af95351b6b9a7c19e25cfd
nikagos <nikagos@hotmail.com>
2024-10-12 21:26:02 +0000

    changed name of the work pool

--- test_deployment_deploy.py
+++ test_deployment_deploy.py
@@ -8,7 +8,7 @@ def my_flow():
 if __name__ == "__main__":
     my_flow.deploy(
         name="my-second-deployment",
-        work_pool_name="test-work-processor", # created in advance
+        work_pool_name="my-work-pool", # created in advance
         image="my-image",
         push=False
         #, cron="* * * * *""
