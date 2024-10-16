6f513aa1e19c8a131f270c612b5bcf183c61c647
nikagos <nikagos@hotmail.com>
2024-10-16 00:10:18 +0000

    added files for docker-based prefect deployment testing

---
+++ 02-workflow-orchestration/git-deploy-parameterized-flow.py
@@ -0,0 +1,24 @@
+from prefect import flow
+from prefect.deployments.runner import DockerImage
+from parameterized_flow import etl_parent_flow
+
+
+@flow(log_prints=True)
+def my_flow():
+    etl_parent_flow()
+
+
+if __name__ == "__main__":
+    my_flow.from_source(
+        source="https://github.com/nikagos/de-zoomcamp-personal-codebase.git",
+        entrypoint="02-workflow-orchestration/parameterized_flow.py:etl_parent_flow"
+    ).deploy(
+        name="parameterized-flow-deployment",
+        work_pool_name="my-work-pool",
+        image=DockerImage(
+            name="my-parameterized-flow-image",
+            tag="v001",
+            dockerfile="Docker-parameterized-flow"
+        ),
+        push=True
+    )
