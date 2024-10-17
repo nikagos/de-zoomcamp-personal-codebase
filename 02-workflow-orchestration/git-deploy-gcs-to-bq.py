261c0aae424ae7aa8062a176bb2eec7434d32c69
nikagos <nikagos@hotmail.com>
2024-10-17 01:50:11 +0000

    fixed script name

--- 02-workflow-orchestration/git-deploy-gcs-to-bq.py
+++ 02-workflow-orchestration/git-deploy-gcs-to-bq.py
@@ -11,7 +11,7 @@ def my_flow():
 if __name__ == "__main__":
     my_flow.from_source(
         source="https://github.com/nikagos/de-zoomcamp-personal-codebase.git",
-        entrypoint="02-workflow-orchestration/etl_gcs_to_bq_parent_flow.py:etl_gcs_to_bq_parent_flow"
+        entrypoint="02-workflow-orchestration/etl_gcs_to_bq_dockerized.py:etl_gcs_to_bq_parent_flow"
     ).deploy(
         name="etl-gcs-to-bq-flow-deployment",
         work_pool_name="my-work-pool",
