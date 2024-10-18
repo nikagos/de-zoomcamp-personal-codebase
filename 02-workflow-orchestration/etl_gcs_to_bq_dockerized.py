cfce03bc7c478dbe52974725f83ceb68877c28cf
nikagos <nikagos@hotmail.com>
2024-10-18 01:20:15 +0000

    changed path

--- 02-workflow-orchestration/etl_gcs_to_bq_dockerized.py
+++ 02-workflow-orchestration/etl_gcs_to_bq_dockerized.py
@@ -17,49 +17,49 @@ def extract_from_gcs(year: int, month: int) -> Path:
     # gcs_block.get_directory(from_path=gcs_path, local_path=f"./data_to_bq")
     # return Path(f"./data_to_bq/{gcs_path}")
     
-    # gcs_block.get_directory(from_path=gcs_path, local_path=f"./")
-    # print(f"./{gcs_path}")
-    # return Path(f"./{gcs_path}")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"./")
+    print(f"./{gcs_path}")
+    return Path(f"./{gcs_path}")
 
-    gcs_block.get_directory(from_path=gcs_path, local_path=f"../")
-    print(f"../{gcs_path}")
-    return Path(f"../{gcs_path}")
-
-
-# @task(log_prints=True)
-# def transform(path: Path) -> pd.DataFrame:
-#     """Data cleaning example"""
-#     df = pd.read_parquet(path)
-#     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-#     df["passenger_count"].fillna(0, inplace=True)
-#     print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
-#     return df
+    # gcs_block.get_directory(from_path=gcs_path, local_path=f"../")
+    # print(f"../{gcs_path}")
+    # return Path(f"../{gcs_path}")
 
 
-# @task(log_prints=True)
-# def write_bq(df: pd.DataFrame, year: int, month: int) -> None:
-#     """Write DataFrame to BiqQuery"""
+@task(log_prints=True)
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    df = pd.read_parquet(path)
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    df["passenger_count"].fillna(0, inplace=True)
+    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
 
-#     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
 
-#     destination_table=f"dezoomcamp.yellow_tripdata_{year}-{month:02}_clean"
-#     print(f"Destination table: {destination_table}")
+@task(log_prints=True)
+def write_bq(df: pd.DataFrame, year: int, month: int) -> None:
+    """Write DataFrame to BiqQuery"""
 
-#     df.to_gbq(
-#         destination_table=destination_table,
-#         project_id="steady-webbing-436216-k1",
-#         credentials=gcp_credentials_block.get_credentials_from_service_account(),
-#         chunksize=500_000,
-#         if_exists="append",
-#     )
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+
+    destination_table=f"dezoomcamp.yellow_tripdata_{year}-{month:02}_clean"
+    print(f"Destination table: {destination_table}")
+
+    df.to_gbq(
+        destination_table=destination_table,
+        project_id="steady-webbing-436216-k1",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
 
 
 @flow(log_prints=True)
 def etl_gcs_to_bq(year: int, month: int) -> None:
     path = extract_from_gcs(year, month)
-    # df = transform(path)
-    # write_bq(df, year, month)
-    # print('Process completed successfully.')
+    df = transform(path)
+    write_bq(df, year, month)
+    print('Process completed successfully.')
 
 
 @flow(log_prints=True)
